cd6ac8d8e65b16e5deeee8fd7f2cefbe3ef52172
HannesNeyer <hannes.neyer@student.htldornbirn.at>
2022-09-21 10:45:38 +0200

    initial

---
+++ src/If_1.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class If_1
+{
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(100);
+
+        if(randomNumber < 20)
+        {
+            System.out.println("mini");
+        }
+        else if(randomNumber > 19 & randomNumber < 51)
+        {
+            System.out.println("medium");
+        }
+        else
+        {
+            System.out.println("large");
+        }
+    }
+}
